49a6c129aee47c996e7805a2fd69580b6f3cd9dd
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-04-26 21:16:09 -0400

    skill bars functionality and css

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,14 @@
     menu.classList.toggle('is-active');
     menuList.classList.toggle('active');
   });
+
+jQuery(".skillbar").each(function () {
+  jQuery(this)
+    .find(".skillbar-bar")
+    .animate(
+      {
+        width: jQuery(this).attr("data-percent"),
+      },
+      2000
+    );
+});
