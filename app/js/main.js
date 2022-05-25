6d3155d02675323cee3f0ccf785fc503151420a3
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-05-25 18:00:23 -0400

    add nav links functionality

--- app/js/main.js
+++ app/js/main.js
@@ -18,38 +18,28 @@ jQuery(".skillbar").each(function () {
     );
 });
 
-$(function () {
-  $(".carousel-item").eq(0).addClass("active");
-  var total = $(".carousel-item").length;
-  var current = 0;
-  $("#moveRight").on("click", function () {
-    var next = current;
-    current = current + 1;
-    setSlide(next, current);
-  });
-  $("#moveLeft").on("click", function () {
-    var prev = current;
-    current = current - 1;
-    setSlide(prev, current);
-  });
-  function setSlide(prev, next) {
-    var slide = current;
-    if (next > total - 1) {
-      slide = 0;
-      current = 0;
-    }
-    if (next < 0) {
-      slide = total - 1;
-      current = total - 1;
-    }
-    $(".carousel-item").eq(prev).removeClass("active");
-    $(".carousel-item").eq(slide).addClass("active");
-    setTimeout(function () {}, 800);
+const sections = document.querySelectorAll("section[id]");
 
-    console.log("current " + current);
-    console.log("prev " + prev);
-  }
-});
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link'");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 
 
